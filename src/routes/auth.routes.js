25d333a26c972f5fc123e607cc8cc686ec045525
N1liaq <ailinyanos83@gmail.com>
2026-09-10 12:32:19 -0300

    feat: se agregaron la estructura general de los controladores junto con sus rutas

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { login, logout, register } from "../controllers/auth.controller.js";
+
+export const authRouter = Router();
+
+authRouter.post("/login", login);
+authRouter.post("/register", register);
+
+authRouter.get("/logout", logout);
